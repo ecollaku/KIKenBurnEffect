2f76de59ea1c6a663c8b2aeba3f79e187ec70b84
e.collaku@kigroup.de <e.collaku@kigroup.de>
2018-09-09 22:12:04 +0200

    Initial Commit

---
+++ KIKenBurnEffect/KIKenBurnEffect.h
@@ -0,0 +1,19 @@
+//
+//  KIKenBurnEffect.h
+//  KIKenBurnEffect
+//
+//  Created by Ergesa Collaku on 09.09.18.
+//  Copyright © 2018 Ergesa Collaku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KIKenBurnEffect.
+FOUNDATION_EXPORT double KIKenBurnEffectVersionNumber;
+
+//! Project version string for KIKenBurnEffect.
+FOUNDATION_EXPORT const unsigned char KIKenBurnEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KIKenBurnEffect/PublicHeader.h>
+
+
